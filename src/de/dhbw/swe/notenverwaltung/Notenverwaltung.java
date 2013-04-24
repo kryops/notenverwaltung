bb301b300447338f9b33487f39711ab70f85b027
Hanne Nobis <Hannenobis@gmx.net>
2013-04-24 11:34:44 +0200

    TODOs für Treiberklassen eingetragen

--- src/de/dhbw/swe/notenverwaltung/Notenverwaltung.java
+++ src/de/dhbw/swe/notenverwaltung/Notenverwaltung.java
@@ -1,5 +1,10 @@
 package de.dhbw.swe.notenverwaltung;
 
 public class Notenverwaltung {
+	//TODO: Noten verwalten: eintragen, bearbeiten, abfragen
+	//TODO: archivieren und löschen
+	//TODO: Bachelornote berechnen
+	
+	
 
 }
--- src/de/dhbw/swe/notenverwaltung/Studentenverwaltung.java
+++ src/de/dhbw/swe/notenverwaltung/Studentenverwaltung.java
@@ -1,5 +1,11 @@
 package de.dhbw.swe.notenverwaltung;
 
 public class Studentenverwaltung {
+	//TODO: Kurse einteilen
+	//TODO: Studenten immatrikulieren (anlegen)
+	//TODO: Studenten verwalten: bearbeiten anzeigen
+	//TODO: Studenten exmatrikulieren
+	
+	
 
 }
