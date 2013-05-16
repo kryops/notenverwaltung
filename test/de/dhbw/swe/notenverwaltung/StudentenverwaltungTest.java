2756cd6ce986fbb9ff199ca0911d161334f67374
Hanne Nobis <Hannenobis@gmx.net>
2013-05-16 16:16:48 +0200

    Testen testen testen ... langweilig!!

---
+++ test/de/dhbw/swe/notenverwaltung/StudentenverwaltungTest.java
@@ -0,0 +1,16 @@
+package de.dhbw.swe.notenverwaltung;
+
+import org.junit.Test;
+
+public class StudentenverwaltungTest extends TestObjekte{
+	
+	
+	@Test
+	public void testKurseErstmalsEinteilen(){
+		
+		
+		
+		
+	}
+
+}
