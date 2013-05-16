829164e6ea10dabae73a083d9235cb4c768f93a6
Hanne Nobis <Hannenobis@gmx.net>
2013-05-16 16:32:08 +0200

    noch mehr tests

--- test/de/dhbw/swe/notenverwaltung/StudentenverwaltungTest.java
+++ test/de/dhbw/swe/notenverwaltung/StudentenverwaltungTest.java
@@ -1,12 +1,18 @@
 package de.dhbw.swe.notenverwaltung;
 
+import static org.junit.Assert.*;
+
 import org.junit.Test;
 
 public class StudentenverwaltungTest extends TestObjekte{
-	
+	DHBW dhbw = DHBW.getDHBW();
+	Studentenverwaltung sv = new Studentenverwaltung();
 	
 	@Test
 	public void testKurseErstmalsEinteilen(){
+		sv.kurseErstmalsEinteilen("kursname1", 2010, "AI", "LLL", "6", studentenAnlegen(5, "zz"));
+		
+		assertEquals("kursname1", dhbw.findKursByName("kursname1"));
 		
 		
 		
